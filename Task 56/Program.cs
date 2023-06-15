0ca855c4b58e0c76948dfa2d513fd5ced48e3d75
Gulchachak <1gulchachak05@mail.ru>
2023-06-16 00:29:00 +0300

    seminar8

---
+++ Task 56/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+const int minVal = 1; 
+const int maxVal = 9; 
+ 
+int[,] FillArrayRandomNumb(int length) { 
+    Random rnd = new Random(); 
+    int[,] randArr = new int[length, length]; 
+ 
+    for (int i = 0; i < length; i++) { 
+        for (int j = 0; j < length; j++) {  
+            randArr[i, j] = rnd.Next(minVal, maxVal); 
+        } 
+    } 
+ 
+    return randArr; 
+} 
+ 
+void PrintArray(int[,] arr) { 
+    for (int i = 0; i < arr.GetLength(0); i++) { 
+        for (int j = 0; j < arr.GetLength(1); j++)  
+            System.Console.Write("{0} ", arr[i, j]); 
+         
+        System.Console.WriteLine(); 
+    } 
+ 
+} 
+ 
+int MinSumRowCalc(int[,] arr) { 
+    int minSumRow = 0; 
+    int minSum = 0; 
+ 
+    for (int i = 0; i < arr.GetLength(0); i++) { 
+        int rowSum = 0; 
+ 
+        for (int j = 0; j < arr.GetLength(1); j++) { 
+            rowSum += arr[i, j]; 
+        } 
+ 
+        if (i == 0 || rowSum < minSum) { 
+            minSum = rowSum; 
+            minSumRow = i + 1; 
+        } 
+    } 
+    return minSumRow; 
+} 
+ 
+System.Console.Write("Введите число m: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+ 
+int[,] arr = FillArrayRandomNumb(m); 
+PrintArray(arr); 
+ 
+int row = MinSumRowCalc(arr); 
+System.Console.WriteLine("номер строки с наименьшей суммой элементов: {0} строка", row);
+
---
+++ Task 60/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+const int length = 2; 
+const int minVal = 10; 
+const int maxVal = 99; 
+ 
+void PrintArrayRandomNumb() { 
+    Random rnd = new Random(); 
+    int[,,] randArr = new int[length, length, length]; 
+ 
+    for (int i = 0; i < length; i++) { 
+        for (int j = 0; j < length; j++) { 
+            for (int k = 0; k < length; k++) { 
+                int randNumb = rnd.Next(minVal, maxVal); 
+                System.Console.Write("{0}({1},{2},{3}) ", randNumb, i, j, k); 
+            } 
+            System.Console.WriteLine();   
+        }   
+    }   
+} 
+ 
+PrintArrayRandomNumb();
+
